d70e03f44a1410b03f1b2796c5837d3192e4519f
Miguel Iran Reyes Uribe <124807939+Gamefriend100@users.noreply.github.com>
2025-11-17 17:29:23 -0600

    Enhance DataSchema with new fields
    Added humiditySensor and nivelAgua fields to DataSchema.

--- backend/models/Data.js
+++ backend/models/Data.js
@@ -1,9 +1,11 @@
+// models/Data.js
 const mongoose = require("mongoose");
 
 const DataSchema = new mongoose.Schema({
-  humedad: Number,
-  temperatura: Number,
-  extra: Object,
+  humedad: Number,         // Humedad del suelo
+  temperatura: Number,     // Temperatura del DHT
+  humedadSensor: Number,   // Humedad del DHT
+  nivelAgua: Number,       // Nivel de agua
   fecha: { type: Date, default: Date.now }
 });
 
