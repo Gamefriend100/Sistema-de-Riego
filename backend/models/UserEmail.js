958ae90c27cf79b19ae5ac95d9bdc71ee86908ab
Miguel Iran Reyes Uribe <124807939+Gamefriend100@users.noreply.github.com>
2025-11-20 08:22:16 -0600

    Delete backend directory

--- backend/models/UserEmail.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserEmailSchema = new mongoose.Schema({
-  email: { type: String, required: true }
-});
-
-module.exports = mongoose.model("UserEmail", UserEmailSchema);
